4e46f43104450af981739ea1fe1ce1935663119a
gabrielgqueiroz <ggabriel12.gg@gmail.com>
2023-09-16 00:41:06 -0300

    desafio java básico com verificações utilizando regex

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero;
+        String agencia, nomeCliente;
+        double saldo;
+        String buff;
+        Scanner scan = new Scanner(System.in);
+
+        do{
+        System.out.println("Por favor, digite o número da Conta:");
+        buff = scan.nextLine();
+        } while(!buff.matches("[0-9]+") || buff.length() > 4);
+        numero = Integer.parseInt(buff);
+
+        do {
+            System.out.println("Por favor, digite o número da Agência:");
+            buff = scan.nextLine(); 
+        } while (!buff.matches("[0-9]{3}-[0-9]"));
+        agencia = buff;
+
+        do{
+            System.out.println("Por favor, digite o nome do dono da Conta:");
+            buff = scan.nextLine();
+        }while(!buff.matches("^[a-zA-Z ]*$"));
+        nomeCliente = buff;
+
+        do{
+            System.out.println("Por favor, digite o saldo da Conta:");
+            buff = scan.nextLine();
+        } while(!buff.matches("^\\d*\\,?\\d{0,2}$"));
+        saldo = Double.parseDouble(buff);
+        scan.close();
+
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque");
     }
 }
